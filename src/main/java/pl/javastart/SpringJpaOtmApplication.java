6082498ec5131ee9f9ce2f6ca823b7ee64f9bcb4
tstepnik <tomasz.stepnik77@gmail.com>
2020-03-12 11:19:19 +0100

    add gitignore and spring initializer files

---
+++ src/main/java/pl/javastart/SpringJpaOtmApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaOtmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaOtmApplication.class, args);
+    }
+
+}
